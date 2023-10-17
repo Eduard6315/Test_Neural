0eef87d659eb3d52171a42ac77a8a553cd561a0c
30056687 <gosha_5@list.ru>
2023-10-17 14:05:59 +0300

    Срздан тест нейросети.

---
+++ cifar10cnn_example.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def train_model():
+    # Загрузка датасета CIFAR-10
+    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
+    # Преобразование меток в one-hot кодировку
+    num_classes = 10
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    # Нормализация данных
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+
+    # Определение архитектуры модели
+    model = keras.Sequential(
+        [
+            layers.Conv2D(32, (3, 3), activation="relu", input_shape=(32, 32, 3)),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(64, (3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Flatten(),
+            layers.Dense(64, activation="relu"),
+            layers.Dense(num_classes, activation="softmax"),
+        ]
+    )
+
+    # Компиляция модели
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+    # Обучение модели
+    history = model.fit(x_train, y_train, batch_size=64, epochs=10, validation_split=0.1)
+
+    # Визуализация процесса обучения
+    plt.plot(history.history["accuracy"], label="Train Accuracy")
+    plt.plot(history.history["val_accuracy"], label="Validation Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.show()
+
+
+
+
